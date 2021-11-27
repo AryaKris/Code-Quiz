a6c95827e9249cf4d8c232d0c8308a1acbf394e6
Arya Krishna <dr.aryakris@outlook.com>
2021-11-27 11:32:33 -0800

    timer to show remaining time after the quiz as final score

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,17 +1,48 @@
 var currentQuestionIndex = 0;
 var totalQuestions = questions.length;
-var timeleft = 60;
+var timeleft = 75;
+var timeInterval = 0;
 var timeEl = document.querySelector("#timer");
 // query selector Returns the first child element that matches a specified CSS selector(s) of an element
 var welcometxt = document.querySelector(".welcome-container");
+var correctAnswersCount = 0;
+
 // Adding an event listener where the user can click the start button and will be taken to the questions section
-document.getElementById("start-button").addEventListener("click",showQuestion );
+document.querySelector("#start-button").addEventListener("click", function () {
+    if (timeInterval === 0) {
+        timeInterval = setInterval(function () {
+            timeleft--;
+            console.log("startTimer: ", currentQuestionIndex, " ", (totalQuestions - 1), " ", timeleft);
+            // if timeleft is > zero, but all questions have been answered
+            if (timeleft > 0 && (currentQuestionIndex > (totalQuestions - 1))) {
+                // if all questions are wrong. timeleft is set to zero else timeleft is the remaining
+                if (correctAnswersCount === 0) {
+                    timeleft = 0;
+                    timeEl.innerHTML = timeleft + " seconds left";
+                } else {
+                    timeEl.innerHTML = timeleft + " seconds remaining";
+                }
+                viewResultPage(timeleft);
+                clearInterval(timeInterval);
+            }
+            else if (timeleft <= 0 || (currentQuestionIndex > (totalQuestions - 1))) {
+                timeleft = 0;
+                timeEl.innerHTML = timeleft + " seconds left";
+                // pass this timeLeft to viewResultPage
+                viewResultPage(timeleft);
+                clearInterval(timeInterval);
+            } else {
+                timeEl.innerHTML = timeleft + " seconds remaining";
+            }
+        }, 1000);
+    }
+    showQuestion();
+});
+
 // Adding an event listener for the user to click the view scores button to view the saved scores
 document.getElementById("view-scores").addEventListener("click", viewScores);
 
 function showQuestion() {
-    startTimer();    
-    
     // inner html Sets or returns the content of an element
     welcometxt.innerHTML = ""
     var choiceSize = questions[currentQuestionIndex].multiChoice.length;
@@ -20,76 +51,52 @@ function showQuestion() {
     welcometxt.appendChild(questionEl);
 
     var str_button = document.querySelector(".welcome-container");
-// for loop for showing the multiple choices for the question
-    for (i = 0; i < choiceSize; i++) {
+    // for loop for showing the multiple choices for the question
+    for (var i = 0; i < choiceSize; i++) {
         var answerchoice = document.createElement("button");
         answerchoice.innerHTML = questions[currentQuestionIndex].multiChoice[i];
         str_button.appendChild(answerchoice);
-        answerchoice.onclick = checkAnswer;
-    }
-}
+        answerchoice.addEventListener("click", function (event) {
 
-function checkAnswer(event) {
-    if (currentQuestionIndex > totalQuestions - 1) {
-        return;
+            if (currentQuestionIndex > totalQuestions - 1) {
+                return;
+            }
+            else {
+                if (questions[currentQuestionIndex].answer !== event.target.textContent) {
+                    console.log("wrong answer selected: ", timeleft);
+                    timeleft -= 10;
+                    console.log("new time: ", timeleft);
+                } else {
+                    correctAnswersCount++;
+                }
+                currentQuestionIndex++;
+                if (currentQuestionIndex <= totalQuestions - 1) {
+                    showQuestion();
+                }
+            }
+        });
     }
-    else {
-        if (questions[currentQuestionIndex].answer !== event.target.textContent) {
-            console.log("wrong answer selected: ", timeleft);
-            timeleft -= 10;
-            console.log("new time: ", timeleft);
-            setTimeout(function () {
-                result.textContent = "";
-            }, 500);
-        }
-        currentQuestionIndex++;
-        showQuestion();
-    }
-}
-
-function startTimer() {
-    
-    var timeInterval = setInterval(function () {    
-        timeleft--;
-        console.log("startTimer: ", currentQuestionIndex, " ", (totalQuestions - 1), " ", timeleft);
-        if (timeleft <= 0 || (currentQuestionIndex > (totalQuestions -1))) {            
-            timeEl.innerHTML = timeleft + " seconds left";
-            viewResultPage();
-            clearInterval(timeInterval);
-        } else {
-            timeEl.innerHTML = timeleft + " seconds remaining";
-        }
-        
-    }, 1000);
 }
 
 function viewResultPage() {
-    console.log("viewResult Page: ", currentQuestionIndex, " ",timeleft);
-    
+    console.log("viewResult Page: ", currentQuestionIndex, " ", timeleft);
+    // inner html Sets or returns the content of an element
     welcometxt.innerHTML = ""
-    var allDoneEl = document.createElement("div");
-    allDoneEl.textContent = "All Done!";
-    allDoneEl.setAttribute("style","font-size:50px; text-align:center;");
+    var allDoneEl = document.createElement("span");
+    allDoneEl.innerHTML = "Quiz is over and final score is: " + timeleft;
     welcometxt.appendChild(allDoneEl);
-    var createForm = document.createElement("FORM");
-    createForm.setAttribute("id", "myForm");
-    allDoneEl.appendChild(createForm);
 }
-function viewScores() {
 
-
-}
 // when the user clocks the viewscores button he should be able to see the stored value of the highscores
 var score = 0;
-var highScores =[];
+var highScores = [];
 
+function viewScores() {
 
+}
 
 // in view score form, get the value of the high score from the session storage and show it here in the new function
 function storeHighScore() {
     // stringify and set key in local storage to highscores array
-    var storedHighscores = JSON.parse(localStorage.getItem("highSCores"));
-
+    var storedHighscores = JSON.parse(localStorage.getItem("highScores"));
 }
-
-
