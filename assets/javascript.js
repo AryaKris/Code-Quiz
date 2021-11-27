08af9642da338f93d696928597260a2380561fa6
Arya Krishna <dr.aryakris@outlook.com>
2021-11-26 23:05:05 -0800

    fixing timer, manipulating the welcome page to show all done when the quiz is done

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,22 +1,26 @@
 var currentQuestionIndex = 0;
-var questionChoice = questions.length;
+var totalQuestions = questions.length;
+var timeleft = 60;
+var timeEl = document.querySelector("#timer");
+// query selector Returns the first child element that matches a specified CSS selector(s) of an element
+var welcometxt = document.querySelector(".welcome-container");
 // Adding an event listener where the user can click the start button and will be taken to the questions section
 document.getElementById("start-button").addEventListener("click",showQuestion );
+// Adding an event listener for the user to click the view scores button to view the saved scores
+document.getElementById("view-scores").addEventListener("click", viewScores);
 
 function showQuestion() {
-    startTimer();
-    // query selector Returns the first child element that matches a specified CSS selector(s) of an element
-    var welcometxt = document.querySelector(".welcome-container");
+    startTimer();    
+    
     // inner html Sets or returns the content of an element
     welcometxt.innerHTML = ""
-    console.log("currentQuestionIndex: ", currentQuestionIndex);
     var choiceSize = questions[currentQuestionIndex].multiChoice.length;
     var questionEl = document.createElement("span");
     questionEl.innerHTML = questions[currentQuestionIndex].title;
     welcometxt.appendChild(questionEl);
 
     var str_button = document.querySelector(".welcome-container");
-
+// for loop for showing the multiple choices for the question
     for (i = 0; i < choiceSize; i++) {
         var answerchoice = document.createElement("button");
         answerchoice.innerHTML = questions[currentQuestionIndex].multiChoice[i];
@@ -26,28 +30,66 @@ function showQuestion() {
 }
 
 function checkAnswer(event) {
-    if (!questions[currentQuestionIndex].answer === event.target.textContent) {
-        console.log("wrong answer selected");
-        time = time - 10;
+    if (currentQuestionIndex > totalQuestions - 1) {
+        return;
     }
-    console.log("currentQuestionIndex_1: ", currentQuestionIndex);
-    if (currentQuestionIndex < questionChoice-1){
+    else {
+        if (questions[currentQuestionIndex].answer !== event.target.textContent) {
+            console.log("wrong answer selected: ", timeleft);
+            timeleft -= 10;
+            console.log("new time: ", timeleft);
+            setTimeout(function () {
+                result.textContent = "";
+            }, 500);
+        }
         currentQuestionIndex++;
         showQuestion();
     }
 }
 
-var timeleft = 75;
-var timeEl = document.querySelector("#timer");
 function startTimer() {
-    setInterval(function () {
-        if (timeleft <= 0) {
-            clearInterval(startTimer);
-            timeEl.innerHTML = "Finished";
+    
+    var timeInterval = setInterval(function () {    
+        timeleft--;
+        console.log("startTimer: ", currentQuestionIndex, " ", (totalQuestions - 1), " ", timeleft);
+        if (timeleft <= 0 || (currentQuestionIndex > (totalQuestions -1))) {            
+            timeEl.innerHTML = timeleft + " seconds left";
+            viewResultPage();
+            clearInterval(timeInterval);
         } else {
             timeEl.innerHTML = timeleft + " seconds remaining";
         }
-        timeleft -= 1;
+        
     }, 1000);
 }
+
+function viewResultPage() {
+    console.log("viewResult Page: ", currentQuestionIndex, " ",timeleft);
+    
+    welcometxt.innerHTML = ""
+    var allDoneEl = document.createElement("div");
+    allDoneEl.textContent = "All Done!";
+    allDoneEl.setAttribute("style","font-size:50px; text-align:center;");
+    welcometxt.appendChild(allDoneEl);
+    var createForm = document.createElement("FORM");
+    createForm.setAttribute("id", "myForm");
+    allDoneEl.appendChild(createForm);
+}
+function viewScores() {
+
+
+}
+// when the user clocks the viewscores button he should be able to see the stored value of the highscores
+var score = 0;
+var highScores =[];
+
+
+
+// in view score form, get the value of the high score from the session storage and show it here in the new function
+function storeHighScore() {
+    // stringify and set key in local storage to highscores array
+    var storedHighscores = JSON.parse(localStorage.getItem("highSCores"));
+
+}
+
 
